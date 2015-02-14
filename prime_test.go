e5ffa4d793fa71eed4d8980ec55349b24c87542e
SeukWon Kang <kasworld@gmail.com>
2015-02-14 14:33:40 +0900

    test update

--- prime_test.go
+++ prime_test.go
@@ -9,18 +9,3 @@ func TestMakePrime(t *testing.T) {
 	t.Logf("%v", len(primes))
 	t.Logf("%v", primes[len(primes)-50:])
 }
-
-func TestGetFactor(t *testing.T) {
-	primes := MakePrimes(0xffff)
-	for i := 0; i < 10; i++ {
-		factors := GetFactor(primes, 50)
-		t.Logf("factors %v", factors)
-		for j := 0; j < 3; j++ {
-			for k := j + 1; k < 3; k++ {
-				if factors[j] == factors[k] {
-					t.Fail()
-				}
-			}
-		}
-	}
-}
